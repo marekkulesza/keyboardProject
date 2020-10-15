b4b406214941f2af551dc07069f82661c65ecd66
marekkulesza <66383506+marekkulesza@users.noreply.github.com>
2020-10-14 23:14:17 -0400

    Add files via upload

--- Virtual_piano/savetime.py
+++ Virtual_piano/savetime.py
@@ -1,20 +1,20 @@
 letterList = "WETYU"
 
 # for element in letterList:
-#     print('event.code != "Key'+element+'" && ',end='')
+#      print('event.code != "Key'+element+'" && ',end='')
 
-# for element in letterList:
-#     print("<kbd>"+element+"</kbd>")
+#  for element in letterList:
+#      print("<kbd>"+element+"</kbd>")
 
-# for element in letterList:
-#     print('<kbd id="'+element+'">'+element+"</kbd>")
+#  for element in letterList:
+#      print('<kbd id="'+element+'">'+element+"</kbd>")
 
-# for element in letterList:
-#     print("""document.addEventListener("keydown", function(event) {
-#     if (event.code == "Key"""+element+"""") 
-#     {
-#         console.log("The '"""+element+"""' key was pressed");
-#         let audio = new Audio("audio"""+element+""".mp3");
-#         audio.play();
-#     }
-#     });""")
+#  for element in letterList:
+#      print("""document.addEventListener("keydown", function(event) {
+#      if (event.code == "Key"""+element+"""") 
+#      {
+#          console.log("The '"""+element+"""' key was pressed");
+#          let audio = new Audio("audio"""+element+""".mp3");
+#          audio.play();
+#      }
+#      });""")
